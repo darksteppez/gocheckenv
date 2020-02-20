4faa078fd8c3445cac1008eb05405af3d42430aa
darksteppez <chris@3rcinteractive.com>
2020-02-20 15:53:55 -0500

    update comment for CheckEnv function

--- gocheckenv.go
+++ gocheckenv.go
@@ -4,7 +4,8 @@ import (
 	"os"
 )
 
-// CheckEnv looks for required environment variables and exits if any are missing
+// CheckEnv looks for required environment variables and returns a slide of strings that includes all missing variables. Len 0 slice means
+// no variables are missing.
 func CheckEnv(vars []string) []string {
 	missing := []string{}
 
